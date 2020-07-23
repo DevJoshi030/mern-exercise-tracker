d968aab9084516535513d236c17f06e46be88fc2
Dev Joshi <devjoshi030@gmail.com>
2020-07-23 18:52:12 +0530

    Updates

---
+++ precache-manifest.ce6c08391979cb618c52d17f19eabbff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de106a2f05cc2f47d6f4bba3cb53266c",
+    "url": "/mern-exercise-tracker/index.html"
+  },
+  {
+    "revision": "2076967d3dacfee81000",
+    "url": "/mern-exercise-tracker/static/css/2.12f1a8df.chunk.css"
+  },
+  {
+    "revision": "2076967d3dacfee81000",
+    "url": "/mern-exercise-tracker/static/js/2.4598c46e.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/mern-exercise-tracker/static/js/2.4598c46e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa53bdd134e9dba34426",
+    "url": "/mern-exercise-tracker/static/js/main.1509dc38.chunk.js"
+  },
+  {
+    "revision": "973bbe73b3a9e0cade15",
+    "url": "/mern-exercise-tracker/static/js/runtime-main.a5d3fb4f.js"
+  }
+]);
