ad9dec3bc04923537591748d0135a52e33b10884
Dev Joshi <devjoshi030@gmail.com>
2020-07-23 15:07:11 +0530

    build package

---
+++ build/precache-manifest.79a73a5405ab3aa89ff52ec3da66ea6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cae815b5b0342ad89d5d22c1992b4e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2076967d3dacfee81000",
+    "url": "/static/css/2.12f1a8df.chunk.css"
+  },
+  {
+    "revision": "2076967d3dacfee81000",
+    "url": "/static/js/2.4598c46e.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.4598c46e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa53bdd134e9dba34426",
+    "url": "/static/js/main.1509dc38.chunk.js"
+  },
+  {
+    "revision": "a37af620878b53014277",
+    "url": "/static/js/runtime-main.0af7428b.js"
+  }
+]);
